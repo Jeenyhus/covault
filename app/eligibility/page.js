82333fcc39d2300b24f32d87419cbb3209faccc9
Dabwitso Maweemba <mweembadabwitso@gmail.com>
2024-11-08 17:39:25 +0200

    Importing eligibility checker component

---
+++ app/eligibility/page.js
@@ -0,0 +1,9 @@
+import EligibilityChecker from '../components/EligibilityChecker';
+
+export default function EligibilityPage() {
+  return (
+    <div className="container mx-auto py-10">
+      <EligibilityChecker />
+    </div>
+  );
+}
