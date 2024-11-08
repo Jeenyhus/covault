541174e5d92bef47b25b86e9c6d89f7b5fe34c25
Dabwitso Maweemba <mweembadabwitso@gmail.com>
2024-11-08 17:41:22 +0200

    importing repayments

---
+++ app/calculator/page.js
@@ -0,0 +1,9 @@
+import RepaymentCalculator from '../components/RepaymentCalculator';
+
+export default function CalculatorPage() {
+  return (
+    <div className="container mx-auto py-10">
+      <RepaymentCalculator />
+    </div>
+  );
+}
