3a10f86fdd35fcb249d0de262dfa58ee2bc495af
Dabwitso Maweemba <mweembadabwitso@gmail.com>
2024-11-09 19:37:49 +0200

    Added loan application form

---
+++ app/apply/page.js
@@ -0,0 +1,10 @@
+// app/apply/page.js
+import LoanApplicationForm from '../components/LoanApplicationForm';
+
+export default function ApplyPage() {
+  return (
+    <div className="container mx-auto py-10">
+      <LoanApplicationForm />
+    </div>
+  );
+}
