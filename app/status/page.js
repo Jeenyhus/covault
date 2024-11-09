1ea51eae7ea35edd372443092371dc0b65ae240a
Dabwitso Maweemba <mweembadabwitso@gmail.com>
2024-11-09 19:39:06 +0200

    Loan application status

---
+++ app/status/page.js
@@ -0,0 +1,10 @@
+// app/status/page.js
+import LoanStatus from '../components/LoanStatus';
+
+export default function StatusPage() {
+  return (
+    <div className="container mx-auto py-10">
+      <LoanStatus />
+    </div>
+  );
+}
